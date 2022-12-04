16ed5bdbb5f08bb7d14ace13a297949f0f489b17
Mursalin80 <mursalin80@hotmail.com>
2022-12-04 20:11:23 +0500

    work on PWA

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,37 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-57.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-180.png" />
-        <meta name="theme-color" content="#fff" />
+
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        /> */}
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta name="twitter:image" content="/icon-192x192.png" />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="/" />
+        <meta property="og:image" content="/apple-180.png" />
       </Head>
       <body>
         <Main />
